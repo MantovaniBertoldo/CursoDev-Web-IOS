02b9be32f3a5207445cae74db7024eccaf30c32a
Vinicius Mantovani <mantovaniv21@gmail.com>
2023-05-08 20:37:38 -0300

    Aula 08/05
    Aula 08/05

---
+++ Vinicius Mantovani/Aulas JS/Aula JS 08-05/main.js
@@ -0,0 +1,8 @@
+var nota1 = 10;
+var nota2 = 8;
+var nota3 = 8;
+var nota4 = 10;
+
+var media1 = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log("A média do aluno com as notas 4 notas é:"+media1)
