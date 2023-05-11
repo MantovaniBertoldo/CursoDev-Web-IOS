5ca0b8c01fe2fb02b63d5efb649efe267894de66
Vinicius Mantovani <mantovaniv21@gmail.com>
2023-05-10 21:01:38 -0300

    Atualizando aula 10-05
    Atualizando

--- Vinicius Mantovani/Workspace/Aulas JS/Aula JS 09-05/main.js
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 09-05/main.js
@@ -7,7 +7,7 @@ let fruta5 = "Uva";
 
 // Calcular o length de cada fruta
 
-console.log('quantidade de letras' )${fruta1.length},
+console.log("quantidade de letras");
 
 // Extrair os 3 primeiros caracteres de cada fruta
 
@@ -20,4 +20,6 @@ const trimmed3 = fruta3.trim();
 const lower4 = fruta4.toLowerCase();
 
 // Exibir a listagem de frutas em um template string
-console.log(`As frutas são: ${fruta1}, ${fruta2}, ${fruta3}, ${fruta4} e ${fruta5}.`);
+console.log(
+  `As frutas são: ${fruta1}, ${fruta2}, ${fruta3}, ${fruta4} e ${fruta5}.`
+);
---
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 10-05/main.js
@@ -0,0 +1,10 @@
+// impar ou par
+// se a sobra da divisao do numero por 2 for 1 esse numero é par
+// se a sobra da divisao do numero por 2 for 0 esse numero é par
+
+var imparpar = 10
+if(imparpar % 2 == 1){
+  console.log("é impar")
+}else{
+  console.log("é par")
+}
