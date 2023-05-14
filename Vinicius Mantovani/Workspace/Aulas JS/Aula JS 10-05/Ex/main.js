162a5bf8fb833e96140abd0af76eb35f6cc0f7f4
BenzemaMantovani <mantovaniv21@gmail.com>
2023-05-14 09:21:53 -0300

    Atualizando Aula
    Trocando Exercício, Aula Algoritimo

---
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 10-05/Ex/main.js
@@ -0,0 +1,12 @@
+// Lê os dois números inteiros
+var numero1 = parseInt("6");
+var numero2 = parseInt("9");
+
+// Verifica qual é o maior e qual é o menor
+if (numero1 > numero2) {
+  console.log("numero1 é maior que numero2");
+} else if (numero1 < numero2) {
+  console.log("numero1 é menor que numero2");
+} else {
+  console.log("Os números são iguais");
+}
