21e27acd516216421a282d98d4b4bef0ac2468ba
Vinicius Mantovani <mantovaniv21@gmail.com>
2023-05-15 21:03:05 -0300

    Atualizando 15-05
    Atualizando 15-05

---
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 15-05/ex 01/main1.js
@@ -0,0 +1,14 @@
+let array = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995];
+
+console.log(array);
+
+console.log(array.length);
+
+console.log(array[6]);
+
+console.log(typeof array.toString());
+
+console.log(array.join('/'));
+
+array.unshift("IOS");
+console.log(array);
