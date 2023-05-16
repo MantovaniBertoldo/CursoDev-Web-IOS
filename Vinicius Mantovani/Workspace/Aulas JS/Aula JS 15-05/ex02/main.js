47270b4fd46f8a789a00e1631361b5abb04edafd
Vinicius Mantovani <mantovaniv21@gmail.com>
2023-05-15 21:22:05 -0300

    Atualizando 15-05
    Atualizando 15-05

--- Vinicius Mantovani/Workspace/Aulas JS/Aula JS 15-05/ex02/main.js
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 15-05/ex02/main.js
@@ -1,10 +1,10 @@
-let array2 = [["HTML", 1993, "CSS", 1996], ["Bootstrap", 2011, "JS", 1995], ["React", 2013, "Java", 1995]];
+let array2 = [["HTML", 1993, "CSS", 1996], ["Bootstrap", 2011, "JS", 1995], ["React", 2013, 1995, "Java"]];
 
 console.log(array2);
 
 console.log(array2.length);
 
-console.log(array2[1][2]);
+console.log(array2[2][1]);
 
 array2[1][2] = "JavaScript";
 console.log(array2);
