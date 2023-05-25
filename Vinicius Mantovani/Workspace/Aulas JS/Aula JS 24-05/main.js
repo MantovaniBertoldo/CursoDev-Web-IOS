e42d1edccfd51b25135521465c4ae8a76c21efe9
Vinicius Mantovani <mantovaniv21@gmail.com>
2023-05-24 21:54:09 -0300

    Atualizando aula 24-05
    Atualizando aula 24-05

---
+++ Vinicius Mantovani/Workspace/Aulas JS/Aula JS 24-05/main.js
@@ -0,0 +1,14 @@
+window.alert('Olá alunos');
+alert('Olá alunos2');
+
+// JavaScript e DOM
+// objeto document
+console.log(document);
+
+// Acessar informações
+console.clear();
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
